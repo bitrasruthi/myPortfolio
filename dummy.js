e1d402771402b79362e95b4bdc091832ef013dea
bitrasruthi <84864550+bitrasruthi@users.noreply.github.com>
2021-09-20 23:21:47 +0530

    changes slides no.

--- dummy.js
+++ dummy.js
@@ -9,8 +9,8 @@ $('.post-wrapper').slick({
    {
      breakpoint: 1024,
      settings: {
-       slidesToShow: 3,
-       slidesToScroll: 3,
+       slidesToShow: 2,
+       slidesToScroll: 2,
        infinite: true,
        dots: true
      }
